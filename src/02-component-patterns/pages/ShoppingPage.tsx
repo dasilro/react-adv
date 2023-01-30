5f6ef57fcf877bfb40f40a5fde8a87363c3d6b91
DSR <dasilro@gmail.com>
2023-01-30 08:47:26 +0100

    Component compound listo

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,33 @@
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from "../components";
+
+const product = {
+  id: "1",
+  title: "Coffe Mug",
+  img: "./coffee-mug.png",
+};
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>ShoppingPage</h1>
+      <hr />
+      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+        <ProductCard product={product}>
+          <ProductCard.Image img={product.img} />
+          <ProductCard.Title title={"llala"} />
+          <ProductCard.Buttons />
+        </ProductCard>
+        <ProductCard product={product}>
+          <ProductImage img={product.img} />
+          <ProductTitle />
+          <ProductButtons />
+        </ProductCard>
+      </div>
+    </div>
+  );
+};
