c287733ad6d761225d14ad4394b157edaea79ba4
DSR <dasilro@gmail.com>
2023-02-01 08:31:45 +0100

    Component extend styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -3,7 +3,12 @@ import noImage from "../assets/no-image.jpg";
 import styles from "../styles/styles.module.css";
 import { ProductContext } from "./ProductCard";
 
-export const ProductImage = ({ img = "" }) => {
+export interface Props {
+  className?: string;
+  img?: string;
+  style?: React.CSSProperties;
+}
+export const ProductImage = ({ className, img, style }: Props) => {
   const { product } = useContext(ProductContext);
   let imgToShow: string;
   if (img) {
@@ -14,5 +19,12 @@ export const ProductImage = ({ img = "" }) => {
     imgToShow = noImage;
   }
 
-  return <img className={styles.productImg} src={imgToShow} alt="Product" />;
+  return (
+    <img
+      className={`${styles.productImg} ${className}`}
+      style={style}
+      src={imgToShow}
+      alt="Product"
+    />
+  );
 };
--- src/02-component-patterns/components/ProductTitle.tsx
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -2,8 +2,18 @@ import { useContext } from "react";
 import styles from "../styles/styles.module.css";
 import { ProductContext } from "./ProductCard";
 
-export const ProductTitle = ({ title }: { title?: string }) => {
+export interface Props {
+  className?: string;
+  title?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductTitle = ({ title, className, style }: Props) => {
   const { product } = useContext(ProductContext);
   const titleToShow: string = title ? title : product.title;
-  return <span className={styles.productDescription}>{titleToShow}</span>;
+  return (
+    <span className={`${styles.productDescription} ${className}`} style={style}>
+      {titleToShow}
+    </span>
+  );
 };
